253aa29d9b1d21f0dfba08955353ff54a7785b84
Obscur23 <obscuredo@mail.ru>
2018-01-21 19:42:40 +0300

    wrk!

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -27,7 +28,7 @@ String baseUrl;
     public void beforeTest(){
         System.setProperty("webdriwer.gecko.driver", "C:/Users/user/IdeaProjects/Selenium1/drv/geckodriver.exe");
         baseUrl = "http://www.sberbank.ru/ru/person";
-    driver = new FirefoxDriver();
+    driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(60,TimeUnit.SECONDS);
     driver.manage().window().maximize();
     driver.get(baseUrl);
@@ -36,22 +37,26 @@ String baseUrl;
 
     @Test
     public void testInsurance(){
-        System.setProperty("webdriwer.gecko.driver", "C:/Users/user/IdeaProjects/Selenium1/drv/geckodriver.exe");
+       // System.setProperty("webdriwer.gecko.driver", "C:/Users/user/IdeaProjects/Selenium1/drv/geckodriver.exe");
+        /**
       WebElement ele = driver.findElement(By.xpath("//span[contains(text(), 'Застраховать себя')]"));
        Actions action = new Actions(driver);
       action.moveToElement(ele).perform();
-
+*/
 
   //      WebElement elee = driver.findElement(By.xpath("//*[contains(@class,'sbrf-div-list-inner --area bp-area overlays-container')]//*[contains(text(), 'Страхование путешественников')]"));
    //     Actions action2 = new Actions(driver);
 
    //   action2.moveToElement(elee).click();
 
+        driver.findElement(By.xpath("//span[@class='multiline']//*[contains(text(),'Застраховать себя')]")).click();
+        driver.findElement(By.xpath("//div[contains(@class,'sbrf-div-list-inner --area bp-area header_more_nav')]//a[contains(text(),'Страхование путешественников')]")).click();
+    Wait<WebDriver> wait = new WebDriverWait(driver, 5,1000);
+        WebElement onlineBtn = driver.findElement(By.xpath("//div[contains(@data-pid,'SBRF-TEXT-1021974')]"));
+      wait.until(ExpectedConditions.elementToBeClickable(onlineBtn)).click();
 
-//   driver.findElement(By.xpath("//*[contains(@class,'sbrf-div-list-inner --area bp-area header_more_nav')]//*[contains(text(), 'Застраховать себя')]"));
-   driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/table/tbody/tr/td/div/div/div/div/div/div[1]/div[1]/div[4]/div/div/div/ul/li[5]/div/div/div[2]/div/a")).click();
-       // Wait<WebDriver> wait = new WebDriverWait(driver, 5,1000);
-       // wait.until(ExpectedCondition.)
+
+
 
     }
 
